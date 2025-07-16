aa16bcdc7b4ab6d7949a494182399e8eb350c6b9
sanjusabu-swe <sanjusabu@icloud.com>
2025-07-16 20:37:43 +0530

    Initial commit ADD files like python.py, bash.sh, and terraform.tf (no folder, eg:docker)

---
+++ bash.sh
@@ -0,0 +1,3 @@
+echo "Hello World"
+echo
+echo "Hi $(whoami)"
---
+++ python.py
@@ -0,0 +1,6 @@
+print ("Hello World")
+a=int(input("How old are you?: "))
+if a <18:
+    print("You're not an adult")
+else:
+    print("You're an adult")
